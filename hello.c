4f716c6711d403715d13c5c7cd53f5db18b0acc4
shamshayder04 <130349070+shamshayder04@users.noreply.github.com>
2023-04-17 08:35:56 +0000

    ddd, Apr 4/17/2023, YYYY, 8:35 A +0

--- hello.c
+++ hello.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include <cs50.c>
+#include <cs50.h>
 int main(void)
 {
 printf("hello, world \n");
 string name = get_string("whats is your name?");
 
-printf("%s\n", name)
+printf("%s\n", name);
 
 }
